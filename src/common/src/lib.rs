7899c949b73d923ac35057683626caef5cc7d839
Dom Rodriguez <shymega@shymega.org.uk>
2023-07-12 22:40:03 +0100

    fix(TdpControl): Add initial Error enum for TdpControl trait

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -1,2 +1,11 @@
 #![no_main]
 #![no_std]
+
+pub enum TdpControlError {
+    SetError
+}
+
+pub trait TdpControl {
+    fn set_tdp(tdp: i64) -> Result<(), TdpControlError>;
+    fn get_tdp() -> Option<i64>;
+}
