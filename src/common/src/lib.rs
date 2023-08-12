a6b1d1fc31d71a2df9de693dbb4e327ea36d5db0
Dom Rodriguez <shymega@shymega.org.uk>
2023-08-12 18:27:10 +0100

    refactor(common): Document, add & expand error handlers

--- src/common/src/lib.rs
+++ src/common/src/lib.rs
@@ -1,17 +1,46 @@
 // SPDX-FileCopyrightText: The WinLinTDPControl Developers
 //
 // SPDX-License-Identifier: MIT
-
+//! Common (shared) crate for `WinLinTDPControl`.
+#![deny(
+    warnings,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    missing_docs,
+    clippy::all,
+    clippy::cargo,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_import_braces,
+    unused_qualifications,
+    unused_extern_crates,
+    variant_size_differences
+)]
 #![no_main]
 #![no_std]
 
+use thiserror_no_std::Error;
+use anyhow::Result;
+
 pub enum TdpControlError {
-    SetError
+    ControlInterfaceError,
+    GenericDriverError,
+    GetTdpError,
+    SetTdpError,
 }
 
+pub type TdpControlResult<T, E = TdpControlError> = anyhow::Result<T>;
+
 pub trait TdpControl {
-    fn set_tdp(tdp: i64) -> Result<(), TdpControlError>;
-    fn get_tdp() -> Option<i64> {
-        None
+    fn set_tdp(tdp: i64) -> TdpControlResult<()>;
+    fn get_current_tdp() -> TdpControlResult<Option<i64>> {
+        Ok(None)
     }
+    fn is_control_available() -> TdpControlResult<bool> {
+        Ok(false)
+    }
+}
+
+pub mod reexports {
+    //! Re-exports of `WinLinTDPControl` components.
 }
