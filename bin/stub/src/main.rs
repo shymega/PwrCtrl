76ab6d3af87db88acfc5c9ef05822fd81399e768
Dom Rodriguez <shymega@shymega.org.uk>
2023-12-04 23:25:44 +0000

    fix(stub): Fix syntax in stub binary

--- bin/stub/src/main.rs
+++ bin/stub/src/main.rs
@@ -1,5 +1,5 @@
-compile_error!("To use this package, you'll need to run the compiler on the target OS, with the compiler tooling, and select the kernel module you want. Bailing!")
+compile_error!("To use this package, you'll need to run the compiler on the target OS, with the compiler tooling, and select the kernel module you want. Bailing!");
 
 fn main() {
-    unimplemented!();
+    unimplemented!()
 }
